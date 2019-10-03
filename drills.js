88f39c13923ab70827ee1427f3f03f3833f7f7ac
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-03 14:22:13 -0400

    solve reverse list

--- drills.js
+++ drills.js
@@ -15,7 +15,6 @@ function main() {
 }
 
 function display(lls) {
-
   let curr = lls.head;
 
   while (curr !== null) {
@@ -24,59 +23,58 @@ function display(lls) {
   }
 }
 
-function size(lls){
+function size(lls) {
   let node = lls.head;
   let i = 1;
-  while (node.next !== null){
+  while (node.next !== null) {
     i++;
-    node = node.next
+    node = node.next;
   }
-  return i
+  return i;
 }
 
-function isEmpty(lls){
-  if (lls.head.next === null){
-    return true
-  } else return false
+function isEmpty(lls) {
+  if (lls.head.next === null) {
+    return true;
+  } else return false;
 }
 
-function findPrevious(lls, key){
-  let node = lls.head
-  let prev = lls.head
-  if(prev.value === key){
-    return 'Key is first in list'
+function findPrevious(lls, key) {
+  let node = lls.head;
+  let prev = lls.head;
+  if (prev.value === key) {
+    return 'Key is first in list';
   }
-  node = node.next
-  while(node.value !== key){
-    prev = node.next
-    node = prev.next
+  node = node.next;
+  while (node.value !== key) {
+    prev = node.next;
+    node = prev.next;
   }
-  return prev.value
+  return prev.value;
 }
-function findLast(lls){
-  if(isEmpty(list)){
-    return 'Empty list'
+function findLast(lls) {
+  if (isEmpty(list)) {
+    return 'Empty list';
   }
-  let node = lls.head
-  while(node.next !== null){
-    node = node.next
+  let node = lls.head;
+  while (node.next !== null) {
+    node = node.next;
   }
-  return node.value
+  return node.value;
 }
 
 function WhatDoesThisProgramDo(lst) {
   let current = lst.head;
   while (current !== null) {
-      let newNode = current;
-      while (newNode.next !== null) {
-          if (newNode.next.value === current.value) {
-              newNode.next = newNode.next.next;
-          }
-          else {
-              newNode = newNode.next;
-          }
+    let newNode = current;
+    while (newNode.next !== null) {
+      if (newNode.next.value === current.value) {
+        newNode.next = newNode.next.next;
+      } else {
+        newNode = newNode.next;
       }
-      current = current.next;
+    }
+    current = current.next;
   }
 }
 /**
@@ -87,15 +85,32 @@ function WhatDoesThisProgramDo(lst) {
  * if the value of the next node === the value of the current node
  * it sets the next value of the new node to the node 2 ahead
  * else it iterates new node
- * 
+ *
  * IT DELETES DUPLICATE ITEMS
  * Polynomial O n^2
  */
 
 const list = main();
-display(list);
+// display(list);
 //console.log(size(list))
 //console.log(isEmpty(list))
 //console.log(findPrevious(list, 'Hot Dog'))
 //console.log(findLast(list))
 //WhatDoesThisProgramDo(list)
+
+function reverseList(lst) {
+  let curr = lst.head;
+  let next = curr.next;
+  let prev = null;
+  while (next !== null) {
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+    next = next.next;
+  }
+  curr.next = prev;
+  lst.head = curr;
+}
+
+reverseList(list);
+display(list);
